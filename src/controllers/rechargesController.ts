d3cc257cb8a899d2986c4ece2abc26075116d37e
Mari Veríssimo <verissimohgmarianna@gmail.com>
2025-07-02 17:34:34 -0300

    controllers layer edited recharge controller added

---
+++ src/controllers/rechargesController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import * as rechargeService from "../services/rechargeService";
+
+export async function createRecharge(req: Request, res: Response) {
+  const recharge = await rechargeService.createRecharge(req.body);
+  res.status(201).send(recharge);
+}
+
+export async function listRecharges(req: Request, res: Response) {
+  const { number } = req.params;
+  const recharges = await rechargeService.listRecharges(number);
+  res.send(recharges);
+}
