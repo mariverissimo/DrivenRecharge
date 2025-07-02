3a7e2ad461e6a3bc0449b9440ae4d1558952bde1
Mari Veríssimo <verissimohgmarianna@gmail.com>
2025-07-02 16:58:06 -0300

    edited sql file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,20 +1,27 @@
+
 CREATE TABLE carriers (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  code INT NOT NULL
 );
 
 CREATE TABLE phones (
-    id SERIAL PRIMARY KEY,
-    number VARCHAR(20) UNIQUE NOT NULL,
-    name TEXT NOT NULL,
-    description TEXT,
-    document VARCHAR(20) NOT NULL,
-    carrier_id INTEGER REFERENCES carriers(id)
+  id SERIAL PRIMARY KEY,
+  number VARCHAR(20) UNIQUE NOT NULL,
+  name TEXT NOT NULL,
+  description TEXT,
+  document VARCHAR(20) NOT NULL,
+  carrier_id INTEGER REFERENCES carriers(id)
 );
 
 CREATE TABLE recharges (
-    id SERIAL PRIMARY KEY,
-    phone_id INTEGER REFERENCES phones(id),
-    amount NUMERIC(10,2) NOT NULL,
-    created_at TIMESTAMP DEFAULT now()
+  id SERIAL PRIMARY KEY,
+  phone_id INTEGER REFERENCES phones(id),
+  amount NUMERIC(10,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT now()
 );
+
+INSERT INTO carriers (name, code) VALUES ('Vivo', 15);
+INSERT INTO carriers (name, code) VALUES ('Tim', 41);
+INSERT INTO carriers (name, code) VALUES ('Oi', 31);
+INSERT INTO carriers (name, code) VALUES ('Claro', 21);
