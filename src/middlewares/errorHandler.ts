54f70305d110d3f8313a31432c7ee0c009c93a09
Mari Veríssimo <verissimohgmarianna@gmail.com>
2025-07-02 17:43:39 -0300

    middlewares edited

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,5 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export function errorHandler(err: any, req: Request, res: Response, next: NextFunction) {
   console.error(err);
-  res.sendStatus(500);
+
+  if (err.type === "conflict") return res.status(409).send(err.message);
+  if (err.type === "not_found") return res.status(404).send(err.message);
+  return res.sendStatus(500);
 }
